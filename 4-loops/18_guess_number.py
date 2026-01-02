26d9e332b2fc1b9447690351131cee399b43b6c2
Gadankush Yash <yashgadankush45@gmail.com>
2026-01-02 12:00:50 +0530

    Fix condition to check number of tries

--- 4-loops/18_guess_number.py
+++ 4-loops/18_guess_number.py
@@ -8,7 +8,7 @@ while guess != 6 and tries < 5:
   guess = int(input('Guess the number: '))
   tries = tries + 1
 
-if guess != 6:
+if tries != 6:
   print('You ran out of tries.')
 else:
   print('You got it!')
